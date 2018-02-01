44fdaa0b1646d7aa5ebb764e829e410ed1b750f7
MartinBear <bhluum@gmail.com>
2018-02-01 12:49:24 +0100

    MessageForm tests

---
+++ src/__tests__/MessageForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Bot from '../components/Bot/Bot';
+
+describe('<Bot />', () =>{
+  const shallowWrap = shallow(<Bot />);
+
+  it('should create a new message and change state', () =>{
+    const content = 'Fo i heldvede';
+    const shallowWrap = shallow(<Bot />);
+    shallowWrap.instance().onSubmit(content);
+    expect(shallowWrap.state().messages[0].message).toContain('Fo i heldvede');
+  });
+      
+  it('should change state when typing', ()=>{
+    const shallowWrap = shallow(<Bot />);
+    expect(shallowWrap.state('typing')).toBe(false);
+    shallowWrap.instance().sendReply();
+    expect(shallowWrap.state('typing')).toBe(true);
+  });
+});
