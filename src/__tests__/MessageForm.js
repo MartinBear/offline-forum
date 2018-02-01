c189853fb630d7db2a630dfd1b1a044f7674bf73
MartinBear <bhluum@gmail.com>
2018-02-01 13:04:57 +0100

    updated, no fails

--- src/__tests__/MessageForm.js
+++ src/__tests__/MessageForm.js
@@ -5,13 +5,14 @@ import Bot from '../components/Bot/Bot';
 describe('<Bot />', () =>{
   const shallowWrap = shallow(<Bot />);
 
-  it('should create a new message and change state', () =>{
+  it('should create a message', () =>{
     const content = 'Fo i heldvede';
     shallowWrap.instance().onSubmit(content);
     expect(shallowWrap.state().messages[0].message).toContain('Fo i heldvede');
   });
       
   it('should change state when typing', ()=>{
+    const shallowWrap = shallow(<Bot />);
     expect(shallowWrap.state('typing')).toBe(false);
     shallowWrap.instance().sendReply();
     expect(shallowWrap.state('typing')).toBe(true);
